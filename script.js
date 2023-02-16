dc0d5a092c710b4fd875651b0e014a10cb5b6ed2
chandan kumar <ck893607@gmail.com>
2023-02-16 21:51:06 +0530

    final

--- script.js
+++ script.js
@@ -2,34 +2,34 @@
 
 let resume1 = document
   .getElementById("resume-link-1")
-   .addEventListener("click",opendrive);
-//     window.location.assign("https://drive.google.com/file/d/1p8v9YRHPNQ6rQkRQk_PS9NpsTeFT7aDZ/view?usp=share_link")
-//   });
+   .addEventListener("click",()=>{;
+    window.location.assign("https://drive.google.com/file/d/1p8v9YRHPNQ6rQkRQk_PS9NpsTeFT7aDZ/view?usp=share_link")
+  });
 
-//   let resume2 = document
-//   .getElementById("resume-link-2")
-//   .addEventListener("click",()=>{
-//     window.location.assign("https://drive.google.com/file/d/1p8v9YRHPNQ6rQkRQk_PS9NpsTeFT7aDZ/view?usp=share_link")
-//   });
-
-
-function opendrive(){
-  window.open(
-    (href =
-      "https://drive.google.com/file/d/1p8v9YRHPNQ6rQkRQk_PS9NpsTeFT7aDZ/view?usp=share_link")
-  );
- }
-
-    let resume2 = document
+  let resume2 = document
   .getElementById("resume-link-2")
-  .addEventListener("click", openDrive);
+  .addEventListener("click",()=>{
+    window.location.assign("https://drive.google.com/file/d/1p8v9YRHPNQ6rQkRQk_PS9NpsTeFT7aDZ/view?usp=share_link")
+  });
 
-function openDrive() {
-  window.open(
-    (href =
-      "https://drive.google.com/file/d/1p8v9YRHPNQ6rQkRQk_PS9NpsTeFT7aDZ/view?usp=share_link")
-  );
- }
+
+// function opendrive(){
+//   window.open(
+//     (href =
+//       "https://drive.google.com/file/d/1p8v9YRHPNQ6rQkRQk_PS9NpsTeFT7aDZ/view?usp=share_link")
+//   );
+//  }
+
+//     let resume2 = document
+//   .getElementById("resume-link-2")
+//   .addEventListener("click", openDrive);
+
+// function openDrive() {
+//   window.open(
+//     (href =
+//       "https://drive.google.com/file/d/1p8v9YRHPNQ6rQkRQk_PS9NpsTeFT7aDZ/view?usp=share_link")
+//   );
+//  }
 
 
 
