4c793ba6f8333778ea4501ab1129ab6982176796
chandan kumar <ck893607@gmail.com>
2023-03-21 15:51:49 +0530

    scroll up buttton added

--- script.js
+++ script.js
@@ -88,3 +88,24 @@ var typed = new Typed(".typing", {
     
     // scrool footer
     sr.reveal('#footer',{delay:200})  
+
+
+     // Get the button
+let mybutton = document.getElementById("myBtn");
+
+   // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
